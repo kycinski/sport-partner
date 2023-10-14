614fb5e5ea98b23a8d421530537e1b61d39d5004
krzysztofkycia <krzysztof.kycia@ilaresoft.com>
2023-10-14 11:35:34 +0200

    fixed shadow on main_image_button

--- lib/view/widgets/main_image_button.dart
+++ lib/view/widgets/main_image_button.dart
@@ -17,26 +17,38 @@ class MainImageButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       borderRadius: BorderRadius.circular(20),
-      child: Ink(
-        height: MediaQuery.of(context).size.height / 4,
-        width: double.infinity,
+      child: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(imagePath),
-            fit: BoxFit.cover,
-          ),
           borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              offset: const Offset(2, 4),
+              blurRadius: 4,
+            ),
+          ],
         ),
-        child: InkWell(
-          borderRadius: BorderRadius.circular(20),
-          onTap: onClick,
-          child: Center(
-            child: GradientText(
-              text,
-              colors: [Colors.grey.shade300, Colors.white],
-              gradientType: GradientType.linear,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineMedium,
+        child: Ink(
+          height: MediaQuery.of(context).size.height / 4,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: onClick,
+            child: Center(
+              child: GradientText(
+                text,
+                colors: [Colors.grey.shade300, Colors.white],
+                gradientType: GradientType.linear,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
           ),
         ),
